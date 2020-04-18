ce001c077e068fb0f4c86b7ace9cd74aaf169f5f
Rubenvaldo Nascimento <rubo.rfn@gmail.com>
2020-04-18 23:16:58 +0000

    testing

--- mobdev-ca2-2019152/src/app/pages/tabs/tab-routing.module.ts
+++ mobdev-ca2-2019152/src/app/pages/tabs/tab-routing.module.ts
@@ -48,7 +48,7 @@ const routes: Routes = [
                 ]
             },
             {
-                path: 'deaths',
+                path: 'deaths', /* testing*/
                 children: [
                     {
                         path: '',
