c72d023c24557f854b4ee01d77b60199f6151803
Rubenvaldo Nascimento <rubo.rfn@gmail.com>
2020-05-23 23:20:16 +0000

    camera function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
-},];
+},]  
+;
 
 @NgModule({
     imports: [
